df14cbb27676504c4ce161da4b52660d4c64b398
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-03-14 01:06:35 +0900

    🔧🚧 초기 설정 emulator에 작동이 되는 모습까지 확인 됨 eslint나 prettier설정은 수정 할 예정 .gitignore에 파일 추가

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const App: () => React$Node = () => {
   return (
     <View>
-      <Text>Hello world!</Text>
+      <Text>Hello world! </Text>
     </View>
   );
 };
