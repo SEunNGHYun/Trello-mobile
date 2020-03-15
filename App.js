0970acd404decc6b49946cb39034a21b2dfe074e
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-03-15 20:53:06 +0900

    🚧 기본 라우터 진행 중 Main Card Board 페이지를 라우터 추가 eslint 기준 변경

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Main from './components/Main';
+import Board from './components/Board';
+import Card from './components/Card';
+import Login from './components/LoginPage';
 
-const App: () => React$Node = () => {
-  return (
-    <View>
-      <Text>Hello world! </Text>
-    </View>
-  );
-};
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const App = () => (
+  <SafeAreaProvider>
+    <NavigationContainer>
+      <Stack.Navigator name="Main" components={Main} />
+      <Stack.Navigator name="Board" components={Board} />
+      <Stack.Navigator name="Card" components={Card} />
+    </NavigationContainer>
+  </SafeAreaProvider>
+);
 
 export default App;
---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Card() {
+  return (
+    <View>
+      <Text>
+        카드 화면
+      </Text>
+    </View>
+  );
+}
