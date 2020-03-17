78836f33be0f7aff10f7cc032205739db4a6fcda
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-03-17 15:08:32 +0900

    🚧 로그인, 회원가입 페이지 수정 axios설치 서버와의 연결은 아직 작성 되지 않음

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import 'react-native-gesture-handler';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Main from './components/Main';
-import Board from './components/Board';
-import Card from './components/Card';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import Main from './components/Main';
+// import Board from './components/Board';
+// import Card from './components/Card';
 import Login from './components/FirstPages/LoginPage';
 import Signup from './components/FirstPages/SignupPage';
 import First from './components/FirstPages/FirstPage';
