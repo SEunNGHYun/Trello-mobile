e75e2eaca0d56e21fb4b54ac52cd457c781686b2
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-04-30 23:23:35 +0900

    🏇 user edit page logout function  add userPage에 logout 기눙 추가

--- components/UserPage.js
+++ components/UserPage.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View, Text, StyleSheet, TouchableOpacity, AsyncStorage,
+} from 'react-native';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { server } from './utils/server';
+import { LogoutAuth } from './Redux/Reducer';
 
 class UserPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userData: {},
+      userData: {
+        userName: null,
+        userEmail: null,
+      },
+      editBool: false,
+      deleteBool: false,
     };
   }
 
@@ -17,21 +25,67 @@ class UserPage extends Component {
       .then((Res) => this.setState({ userData: Res.data }));
   }
 
+  logout = () => {
+    this.props.logout();
+    AsyncStorage.removeItem('user_Token');
+  }
+
+  edit = () => {
+
+  }
+
+  delete = () => {
+    this.props.logout();
+  }
+
   render() {
+    const { userName, userEmail } = this.state.userData;
     return (
             <View>
                 <View />
-                <Text>
-                    User Edit
+                <Text style={styles.username}>
+                  {userName}
                 </Text>
+                <Text style={styles.useremail}>
+                  {userEmail}
+                </Text>
+                <TouchableOpacity onPress={this.logout}>
+                  <Text>
+                    Logut
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text>
+                    Edit
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Text>
+                    Delete
+                  </Text>
+                </TouchableOpacity>
             </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  username: {
+    fontSize: 40,
+  },
+  useremail: {
+    fontSize: 15,
+  },
+});
+
 const mapStatesToProps = ({ token }) => {
   token;
 };
 
-export default connect(mapStatesToProps)(UserPage);
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => {
+    dispatch(LogoutAuth());
+  },
+});
+
+export default connect(mapStatesToProps, mapDispatchToProps)(UserPage);
